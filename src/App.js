bf977f6592f853b781e66ec5f9bd425200278404
dcheng34551 <dcheng34551@gmail.com>
2021-05-27 13:27:01 +0800

    basic native login and signup

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
+import React, { useState, useEffecct } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import firebase from 'firebase/app';
+import 'firebase/auth';
 import EditPage from './components/Pages/EditPage';
+import LoginPage from './components/Pages/LoginPage';
+import MainPage from './components/Pages/MainPage';
 import './Utils/firebase';
 
 function App() {
+    const [currentUser, setCurrentUser] = useState({ email: null });
+    const [currentUserTimes, setCurrentUserTimes] = useState(0);
+
+    if (currentUserTimes === 0) {
+        firebase.auth().onAuthStateChanged((user) => {
+            user ? setCurrentUser(user) : setCurrentUser({ email: 'noUser' });
+        });
+        setCurrentUserTimes(1);
+    }
     return (
         <Router>
             <div className="App">
@@ -10,6 +24,26 @@ function App() {
                     <Route
                         path="/edit/:id"
                         render={(props) => <EditPage {...props} />}
+                    />
+                    <Route
+                        path="/login"
+                        render={(props) => (
+                            <LoginPage
+                                currentUser={currentUser}
+                                setCurrentUser={setCurrentUser}
+                                {...props}
+                            />
+                        )}
+                    />
+                    <Route
+                        path="/main/:id"
+                        render={(props) => (
+                            <MainPage
+                                currentUser={currentUser}
+                                setCurrentUser={setCurrentUser}
+                                {...props}
+                            />
+                        )}
                     />
                 </Switch>
             </div>
